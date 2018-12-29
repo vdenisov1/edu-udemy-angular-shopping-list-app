a38331051795e249c9fa33ee8db475c30e82c8df
Vlad Denisov <vlad@vdenisov.net>
2018-12-29 13:08:43 -0500

    Added recipe menu functionality and recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe',
                'This is simple a test',
               'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Tomato_Bruschetta.jpg/800px-Tomato_Bruschetta.jpg'),
-    new Recipe('A Test Recipe',
+    new Recipe('A Test Recipe 2',
       'This is simple a test',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Tomato_Bruschetta.jpg/800px-Tomato_Bruschetta.jpg')
   ];
@@ -19,6 +21,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
