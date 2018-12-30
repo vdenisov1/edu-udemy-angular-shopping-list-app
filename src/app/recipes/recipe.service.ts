28519cc80f9635be378ebbac04d107ec5867004d
Vlad Denisov <vlad@vdenisov.net>
2018-12-29 19:31:57 -0500

    Added ingredients recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,26 @@
 import { Recipe } from './recipe.model';
 import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
-      'This is simple a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Tomato_Bruschetta.jpg/800px-Tomato_Bruschetta.jpg'),
-    new Recipe('A Test Recipe 2',
-      'This is simple a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Tomato_Bruschetta.jpg/800px-Tomato_Bruschetta.jpg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'An awesome Schnitzel!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'American Style',
+      'https://images.pexels.com/photos/263109/pexels-photo-263109.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Cheese', 2)
+      ]
+    )
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
