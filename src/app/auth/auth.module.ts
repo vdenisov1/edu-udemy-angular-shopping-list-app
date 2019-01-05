802db593ed981c7187bb35487fe1bb60b46cd623
Vlad Denisov <vlad@vdenisov.net>
2019-01-05 12:09:22 -0500

    Refactored features into modules

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { FormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+    AuthRoutingModule,
+    FormsModule
+  ]
+})
+export class AuthModule {}
