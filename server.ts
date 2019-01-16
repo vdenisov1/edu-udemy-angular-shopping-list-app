55850e6ac6775a356142a18578ad54cc86889780
Vladimir Denisov <Vlad@vdenisov.net>
2019-01-16 11:41:40 -0500

    Added angular universal

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // Our index.html we'll use as our template
-const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
+const template = readFileSync(join(DIST_FOLDER, 'function','browser', 'index.html')).toString();
 
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
 
